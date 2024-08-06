81851ce7dac3d8dfbe4d304711ab420fc07e0084
Maks Plevako <maksplevako@gmail.com>
2024-08-07 00:38:58 +0300

    add delete address

--- app/profile/addresses/page.js
+++ app/profile/addresses/page.js
@@ -1,6 +1,7 @@
 import ProfileLayout from '@/app/components/ProfileLayout'
 import { auth } from '@/config/auth'
 import ButtonAddress from '@/app/components/ButtonAddress'
+import ChangeAddress from '@/app/components/ChangeAddress'
 
 export default async function Addresses() {
 	const session = await auth()
@@ -25,7 +26,7 @@ export default async function Addresses() {
 						<div>
 							{houses.map(house => (
 								<div
-									key={house.address}
+									key={house.addressId}
 									className='border rounded-xl border-[#7A7680] p-5 relative my-2'
 								>
 									<div className='font-medium text-[18px]'>
@@ -36,40 +37,10 @@ export default async function Addresses() {
 										м. {house.city}
 									</div>
 									<div className='text-[#55556D]'>Індекс {house.index}</div>
-									<div className='absolute top-3 right-3 flex flex-row gap-2'>
-										<button>
-											<svg
-												xmlns='http://www.w3.org/2000/svg'
-												fill='none'
-												viewBox='0 0 24 24'
-												strokeWidth={1.5}
-												stroke='currentColor'
-												className='size-6 hover:text-[#6B59CC] transit ease-in-out duration-200'
-											>
-												<path
-													strokeLinecap='round'
-													strokeLinejoin='round'
-													d='m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125'
-												/>
-											</svg>
-										</button>
-										<button>
-											<svg
-												xmlns='http://www.w3.org/2000/svg'
-												fill='none'
-												viewBox='0 0 24 24'
-												strokeWidth={1.5}
-												stroke='currentColor'
-												className='size-6 hover:text-[#F11A22] transit ease-in-out duration-200'
-											>
-												<path
-													strokeLinecap='round'
-													strokeLinejoin='round'
-													d='m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0'
-												/>
-											</svg>
-										</button>
-									</div>
+									<ChangeAddress
+										email={session.user.email}
+										addressId={house.addressId}
+									/>
 								</div>
 							))}
 						</div>
@@ -81,7 +52,7 @@ export default async function Addresses() {
 						<div>
 							{posts.map(post => (
 								<div
-									key={post.address}
+									key={post.addressId}
 									className='border rounded-xl border-[#7A7680] p-5 relative my-2'
 								>
 									<div className='font-medium text-[18px]'>
--- pages/api/user/address.js
+++ pages/api/user/address.js
@@ -1,11 +1,12 @@
 import clientPromise from '@/lib/mongodb'
+import { ObjectId } from 'mongodb'
 
 export default async function handler(req, res) {
+	const client = await clientPromise
+	const db = client.db('store')
+
 	if (req.method === 'POST') {
 		try {
-			const client = await clientPromise
-			const db = client.db('store')
-
 			const {
 				type,
 				address,
@@ -17,31 +18,46 @@ export default async function handler(req, res) {
 				email,
 			} = req.body
 
-			// Создаем новый адрес
 			const newAddress = {
 				type,
 				address,
 				street,
-				...(apartment_num && { apartment_num }), // Добавляем поле, если оно существует
-				...(index && { index }), // Добавляем поле, если оно существует
-				...(department_number && { department_number }), // Добавляем поле, если оно существует
+				...(apartment_num && { apartment_num }),
+				...(index && { index }),
+				...(department_number && { department_number }),
 				city,
 			}
 
-			// Обновляем документ пользователя, добавляя новый адрес в массив
-			await db
-				.collection('users')
-				.updateOne({ email: email }, { $push: { addresses: newAddress } })
+			await db.collection('users').updateOne(
+				{ email: email },
+				{
+					$push: { addresses: newAddress },
+				}
+			)
 
 			res.status(200).json({ message: 'Address added successfully' })
 		} catch (error) {
-			console.error(error) // Логируем ошибку для отладки
+			console.error(error)
 			res.status(500).json({ error: 'Failed to add address' })
-		} finally {
-			await client.close() // Закрываем соединение
+		}
+	} else if (req.method === 'DELETE') {
+		try {
+			const { email, addressId } = req.query
+
+			await db
+				.collection('users')
+				.updateOne(
+					{ email: email },
+					{ $pull: { addresses: { addressId: new ObjectId(addressId) } } }
+				)
+
+			res.status(200).json({ message: 'Address deleted successfully' })
+		} catch (error) {
+			console.error(error)
+			res.status(500).json({ error: 'Failed to delete address' })
 		}
 	} else {
-		res.setHeader('Allow', ['POST'])
+		res.setHeader('Allow', ['POST', 'DELETE'])
 		res.status(405).end(`Method ${req.method} Not Allowed`)
 	}
 }
